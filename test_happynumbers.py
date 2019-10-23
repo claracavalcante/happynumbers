0300c6ba477d3db26d64e2ec0bad6f9f17f45bd0
Ana Clara Cavalcante <anaclaracavalcante14@gmail.com>
2019-10-23 06:01:53 -0300

    test is happy with three different numbers

--- test_happynumbers.py
+++ test_happynumbers.py
@@ -1,4 +1,14 @@
 from happynumbers import get_happiness_state
+from random import seed
+from random import randint
 
-def test_is_happy():
+def test_is_happy_one():
+    assert True == get_happiness_state(1)
+
+def test_is_happy_seven():
     assert True == get_happiness_state(7)
+
+def test_is_happy_random_100():
+    seed(1)
+
+    assert True == get_happiness_state(randint(0, 100))
