f97048e878d17d653d72b1bbdae0c91ae6875771
Ana Clara Cavalcante <anaclaracavalcante14@gmail.com>
2019-10-23 05:49:41 -0300

    get happiness state function passing v1

--- happynumbers.py
+++ happynumbers.py
@@ -1,3 +1,18 @@
 
 def get_happiness_state(num):
-    return False
+    res = num
+
+    while (res > 1):
+        digits = [int(d) for d in str(res)]
+        count = 0
+
+        for i in digits:
+            if count == 0: 
+                res = i**2
+                count += 1
+                continue
+            res = res + (i**2)
+            count += 1
+        print(res)
+    return 1
+    return True if (res == 1) else False
