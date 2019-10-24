8b3d2d2bd5d2f083f94cc31f938fa597e66e7781
Ana Clara Cavalcante <anaclaracavalcante14@gmail.com>
2019-10-24 16:33:11 -0300

    remove big number test

--- test_happynumbers.py
+++ test_happynumbers.py
@@ -10,6 +10,3 @@ def test_is_happy_seven():
 
 def test_is_happy_zero():
     assert False == get_happiness_state(0)
-
-def test_is_happy_big_number():
-    assert True == get_happiness_state(28907)
